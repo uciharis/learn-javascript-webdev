6ce3cc8c0bdbe856483fdeb6672777f6627564dc
the swallower <harisnop@gmail.com>
2026-05-10 09:42:39 +0000

    js is case sensitive

--- 1intro.js
+++ 1intro.js
@@ -7,3 +7,8 @@ let danaMBG = 100;
 console.log("nilai " + danaMBG + " | type " + typeof(danaMBG)); // number
 danaMBG = danaMBG.toString();
 console.log("nilai " + danaMBG + " | type:" + typeof(danaMBG));
+
+// js is case sensitive
+menuMBG = 4;
+console.log(menuMBG);
+console.log(menumbg);
