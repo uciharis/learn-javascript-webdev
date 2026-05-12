bf92104d974f77f6c4e69402e47d222b7da73e47
uciha <harisnop@gmail.com>
2026-05-13 00:22:54 +0700

    chapter ke-3 ttg variables - convention

--- 3Variables.js
+++ 3Variables.js
@@ -9,3 +9,21 @@ console.log(1a);
 
 let a1 = 700;
 console.log(a1); // name is valid
+/**
+let 10 = 30; // sintak error, nama variabel terdiri dari angka keseluruhan
+console.log(10);
+ **/
+
+// camelcase di naming variable
+let hargaDasarMGB = 20_000;
+console.log(hargaDasarMGB);
+
+// naming Class name -> PascalCase
+class MyClass {
+    constructor(name) {
+        console.log(name);
+    }
+};
+
+// let, const
+le trumpt  k
